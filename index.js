1a5aa06baafcd009e80e97f52981329da1a019c3
ricardogamer23 <157838443+ricardogamer23@users.noreply.github.com>
2024-01-28 23:34:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Jugando RicardoLandia 😲"];
 
 
 let currentIndex = 0;
